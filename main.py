d38bf69225cda0d363b7b307a7fdf2943ef93765
itsjeremyhsieh <jeremy.study0107@gmail.com>
2024-10-12 01:31:55 +0800

    add option

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ def main():
         print("\nFlight Booking System")
         print("1. Search available tickets")
         print("2. Book a ticket")
-        print("3. Exit")
+        print("3. Refund a ticket")
+        print("4: Exit system")
         choice = input("Enter your choice: ")
         
         if choice == '1':
@@ -14,6 +15,7 @@ def main():
             book_ticket()
         elif choice == '3':
             refund()
+        elif choice == '4':
             break
         else:
             print("Invalid choice. Please try again.")
