73850785ab70a70fb7c9140c5e882d73e408584e
Minami <2698759149@qq.com>
2024-10-21 13:33:18 +0800

    exchange用户输入修改
    将通过用户输出查找机票单独提取为get_user_flight_id

--- presentation/cli.py
+++ presentation/cli.py
@@ -9,19 +9,21 @@ order_repo = OrderRepository(db_connection="data/airline.db")
 payment_gateway = PaymentGateway()
 ticket_use_case = TicketUseCase(flight_repo, payment_gateway, order_repo)
 
+
 def available_tickets():
     departure = input("Enter departure city: ")
     destination = input("Enter destination city: ")
     date = input("Enter departure date (YYYY-MM-DD): ")
     flight_class = input("Enter flight class (Economy/Business): ")
     num_passengers = int(input("Enter number of passengers: "))
-    
-    available_flights = ticket_use_case.search_available_flights(departure, destination, date, flight_class, num_passengers)
 
+    available_flights = ticket_use_case.search_available_flights(
+        departure, destination, date, flight_class, num_passengers)
 
     print("Available flights:")
     for flight in available_flights:
         print(flight)
+
 
 def book_ticket():
     flight_id = int(input("Enter flight ID to book: "))
@@ -34,15 +36,18 @@ def book_ticket():
         'expiry_date': input("Enter card expiry date (MM/YY): "),
         'cvv': input("Enter CVV: ")
     }
-    
+
     connection = flight_repo.connect()
     cursor = connection.cursor()
-    cursor.execute('INSERT OR IGNORE INTO User (id, name, passport_num) VALUES (?, ?, ?)', (user_id, user_name, passport_num))
+    cursor.execute('INSERT OR IGNORE INTO User (id, name, passport_num) VALUES (?, ?, ?)',
+                   (user_id, user_name, passport_num))
     connection.commit()
     connection.close()
-    
-    ticket = ticket_use_case.book_ticket(flight_id, user_id, num_passengers, payment_info)
+
+    ticket = ticket_use_case.book_ticket(
+        flight_id, user_id, num_passengers, payment_info)
     print("Ticket booked:", ticket)
+
 
 def refund():
     user_id = int(input("Enter user ID: "))
@@ -50,9 +55,40 @@ def refund():
     passport_num = input("Enter your passport number: ")
     order_id = int(input("Enter order ID: "))
 
-    result = ticket_use_case.refund_ticket(user_id, user_name, passport_num, order_id)
+    result = ticket_use_case.refund_ticket(
+        user_id, user_name, passport_num, order_id)
     if result:
         print("Ticket refunded.")
     else:
         print("Ticket not refunded due to error.")
+
+
+def exchange_ticket():
+    user_id = int(input("Enter user ID: "))
+    order_id = input("Enter your order ID: ")
+    passport_num = int(input("Enter Passport: "))
+
+    flight_id=ticket_use_case.get_user_flight_id(order_id, passport_num, user_id)
     
+    departure = input("Enter departure city: ")
+    destination = input("Enter destination city: ")
+    date = input("Enter departure date (YYYY-MM-DD): ")
+    flight_class = input("Enter flight class (Economy/Business): ")
+    num_passengers_to_change = int(input("Enter number of passengers to exchange: "))
+    
+    new_flight_id = ticket_use_case.search_available_flights(departure, destination, date, flight_class, num_passengers_to_change)
+    
+    payment_info = {
+        'card_number': input("Enter card number: "),
+        'expiry_date': input("Enter card expiry date (MM/YY): "),
+        'cvv': input("Enter CVV: ")
+    }
+
+    try:
+        new_ticket = ticket_use_case.exchange_ticket(
+            flight_id, new_flight_id, user_id, num_passengers_to_change, payment_info
+        )
+        print("Ticket exchanged successfully:", new_ticket)
+
+    except Exception as e:
+        print(f"Error: {e}")
